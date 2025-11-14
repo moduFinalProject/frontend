835973a8e3cc006af32aa72b85e9cafcca0dfe61
jieunheo <gjekdmlwl@naver.com>
2025-11-14 16:44:10 +0900

    style: 파일 모바일 가운데 정렬

--- src/components/FormElem/file/File.css.ts
+++ src/components/FormElem/file/File.css.ts
@@ -5,4 +5,12 @@ export const btnWrap = style({
   display: "flex",
   flexDirection: "column",
   gap: vars.spacing.sm,
+
+  "@media": {
+    "(max-width: 768px)": {
+      button: {
+        alignSelf: "center",
+      },
+    },
+  },
 });
