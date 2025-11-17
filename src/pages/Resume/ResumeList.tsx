f3964a01cbb2694769573a2d3d4164341b85d2e2
jieunheo <gjekdmlwl@naver.com>
2025-11-17 15:22:04 +0900

    feat: 이력서 스크롤 기능 추가

--- src/pages/Resume/ResumeList.tsx
+++ src/pages/Resume/ResumeList.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { resumeList } from "./ResumeList.css.ts";
 import ResumeItem from "./components/ResumeItem.tsx";
 import Search from "./components/form/Search.tsx";
 import { getResumeList } from "@/services/resumes.ts";
-import { useResumeContext, type ResumeData } from "./ResumeContext.tsx";
+import { useResumeContext, type Resume } from "./ResumeContext.tsx";
+import { Button } from "@/components/index.ts";
 
 export default function ResumeList() {
   const {
@@ -13,39 +14,48 @@ export default function ResumeList() {
     setIsLoading,
     page,
     setPage,
+    search,
   }: {
-    resumes: ResumeData;
-    setResumes: (arg0: boolean) => void;
+    resumes: Resume[];
+    setResumes: (arg0: Resume[] | ((prev: Resume[]) => Resume[])) => void;
     isLoading: boolean;
     setIsLoading: (arg0: boolean) => void;
     page: number;
     setPage: (arg0: number) => void;
+    search: string;
   } = useResumeContext();
+  const [hasMore, setHasMore] = useState(true);
 
-  useEffect(() => {
-    let isMounted = true;
+  const loadResumesData = useCallback(async () => {
+    if (isLoading || !hasMore) return;
 
     setIsLoading(true);
 
-    const loadResumesData = async () => {
-      const data = await getResumeList(page);
+    try {
+      const data = await getResumeList({ page, limit: 6, search });
 
-      if (isMounted) {
-        // 컴포넌트가 마운트된 상태에서만 상태 업데이트
-        if (data) {
-          setResumes(data);
+      if (data && data.length > 0) {
+        setResumes((prev) => [...prev, ...data]);
+        setPage(page + 1);
+
+        if (data.length < 6) {
+          setHasMore(false);
         }
-        setIsLoading(false);
-        setPage((prev) => prev++);
+      } else {
+        setHasMore(false);
       }
-    };
+    } catch (error) {
+      console.error("이력서 목록 로딩 오류:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [page, search, hasMore, setResumes, setIsLoading, isLoading]);
 
-    loadResumesData();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+  useEffect(() => {
+    if (page === 1) {
+      loadResumesData();
+    }
+  }, [loadResumesData]);
 
   return (
     <>
@@ -54,16 +64,36 @@ export default function ResumeList() {
       <div>
         <h3 className="a11y-hidden">이력서 목록</h3>
         <ul className={resumeList}>
-          {isLoading ? (
-            <div>이력서 목록 로딩 중...</div>
-          ) : resumes.length > 0 ? (
-            resumes.map((resumeItem) => (
-              <ResumeItem resume={resumeItem} key={resumeItem.resume_id} />
-            ))
-          ) : (
-            <li>등록된 이력서가 없습니다.</li>
-          )}
+          {resumes.map((resumeItem) => (
+            <ResumeItem
+              resume={resumeItem}
+              key={`${resumeItem.resume_id}-${resumeItem.title}`}
+            />
+          ))}
         </ul>
+        {isLoading && <div>불러오는 중...</div>}
+
+        {!isLoading && resumes.length === 0 && (
+          <div>등록된 이력서가 없습니다.</div>
+        )}
+        {hasMore ? (
+          <div style={{ marginTop: "1rem" }}>
+            <Button
+              text="더보기"
+              color="blue"
+              widthStyle="full"
+              callback={() => {
+                loadResumesData();
+              }}
+            />
+          </div>
+        ) : (
+          resumes.length > 0 && (
+            <div style={{ marginTop: "1rem", textAlign: "center" }}>
+              더 이상 불러올 이력서가 없습니다.
+            </div>
+          )
+        )}
       </div>
     </>
   );
